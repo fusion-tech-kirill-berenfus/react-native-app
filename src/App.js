700110a5d8b6d2268a60a34e6956494a49ee03f4
fusion-tech-kirill-berenfus <kirill.berenfus@fusion-tech.pro>
2022-08-23 14:02:36 +0300

    Add bootsplash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import RNBootSplash from 'react-native-bootsplash';
 
 import StackContainer from './navigation/Stack';
 
 import {persistor, store} from './redux/store';
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true});
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
